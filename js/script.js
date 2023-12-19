c749a99f9b510d1e88eacba2a8940e715e96cc40
DartMaskedGuy <91269918+DartMaskedGuy@users.noreply.github.com>
2023-12-19 13:09:52 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,9 +1,78 @@
-$(document).ready(function(){
-    $('.menu-toggler').click(function(){
-        $(this).toggleClass("active");
-        $(".navbar").toggleClass("active");
-    });
+$(document).ready(function () {
+  $('.menu-toggler').click(function () {
+    $(this).toggleClass("active");
+    $(".navbar").toggleClass("active");
+  });
 });
+
+
+
+
+
+(function () {
+  "use strict";
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  /**
+   * Easy on scroll event listener 
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+  /**
+   * Back to top button
+   */
+  let backtotop = select('.back-to-top')
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add('active')
+      } else {
+        backtotop.classList.remove('active')
+      }
+    }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+  }
+
+
+  /**
+   * Scroll with ofset on page load with hash links in the url
+   */
+  window.addEventListener('load', () => {
+    if (window.location.hash) {
+      if (select(window.location.hash)) {
+        scrollto(window.location.hash)
+      }
+    }
+  });
+
+  /**
+   * Animation on scroll
+   */
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    })
+  });
+
+})()
+
 // Darkmode
 const darkBtn = document.getElementById('dark-btn');
 
@@ -12,20 +81,20 @@ darkBtn.onclick = () => {
   document.body.classList.toggle('dark-theme');
 
   // Checking to see whether if it is in the light mode or dark mode
-  if(localStorage.getItem('theme') == 'light'){
+  if (localStorage.getItem('theme') == 'light') {
     localStorage.setItem('theme', 'dark');
-  }else {
+  } else {
     localStorage.setItem('theme', 'light');
   }
 }
 
 // Local Storage Check
-if(localStorage.getItem('theme') == 'light'){
+if (localStorage.getItem('theme') == 'light') {
   darkBtn.classList.remove('btn-on');
   document.body.classList.remove('dark-theme');
-}else if(localStorage.getItem('theme') == 'dark'){
+} else if (localStorage.getItem('theme') == 'dark') {
   darkBtn.classList.add('btn-on');
   document.body.classList.add('dark-theme');
-}else{
+} else {
   localStorage.setItem('theme', 'light');
 }
